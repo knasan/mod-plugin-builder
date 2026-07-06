107cdc799319173fda6a79cfa5b043c18114a7c3
Sandy Marko Knauer <sandy.marko.k@gmail.com>
2026-07-07 01:08:55 +0200

    Disable libgig in defconfigs
    The SVN source and buildroot mirror are no longer available.
    No in-tree plugin depends on libgig; SF2 plugins use fluidsynth.

--- plugins-dep/package/libgig/libgig.mk
+++ plugins-dep/package/libgig/libgig.mk
@@ -4,6 +4,9 @@
 #
 ################################################################################
 
+# Removed libgig. No in-tree package depends on it.
+# Can be re-enabled if proprietary LinuxSampler/GIG plugins are required.
+
 LIBGIG_VERSION = 2855
 LIBGIG_SITE = https://svn.linuxsampler.org/svn/libgig/trunk
 LIBGIG_SITE_METHOD = svn
